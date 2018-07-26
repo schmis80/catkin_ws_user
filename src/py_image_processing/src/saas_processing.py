fb72328356aa6c4d1725d99102f743d96cd71152
schmis80 <stefan.schmid96@gmail.com>
2018-07-26 17:46:59 +0200

    add failed attempt to recreate real world coordinates for u03

--- src/py_image_processing/src/saas_processing.py
+++ src/py_image_processing/src/saas_processing.py
@@ -17,7 +17,7 @@ ret,thresh1=cv2.threshold(gray, bi_gray_min, bi_gray_max, cv2.THRESH_BINARY);
 cv2.imshow('image',thresh1)
 #cv2.waitKey(0)
 
-class blob:
+class Cluster:
   def __init__(self,i):
     self.num = i
     self.points = []
@@ -28,56 +28,51 @@ class blob:
     self.size += 1
 
   def get_center(self):
-    # returns the (x,y) average of the mean of a blobs points
     mat = np.array(self.points)
     center = np.sum(mat, axis=0)/len(self.points)
     return (center[0], center[1])
 
-# create a list of blobs
-blobs = []
-# create a reference matrix to record blob assignments
+# list of clusters
+clusters = []
+# reference matrix to record cluster assignments
 ref = np.ones(thresh1.shape) * -1
 
 for y in range(thresh1.shape[0]):
     for x in range(thresh1.shape[1]):
-
         # is the pixel we are looking at white?
-        if thresh1[y,x] == bi_gray_max:
-
-            # the radius in which we look for pixels that already belong to a blob                    
+        if thresh1[y,x] >=bi_gray_min and thresh1[y,x] <= bi_gray_max:
+            # radius in which the point will belong to a cluster 
             r = 9
 
-            # look if a neighbour of x,y is already assigned to a blob
+            # look for neighbours, already assigned to a cluster
             try:
-                n_blob = np.max(ref[y-r:y+r,x-r:x+r])            
-            except ValueError:  #raised if `n_blob` is empty.
-                n_blob = -1
+                cluster_num = np.max(ref[y-r:y+r,x-r:x+r])            
+            except ValueError:  #raised if 'cluster_num' is empty.
+                cluster_num = -1
 
-            if n_blob > 0:
-                blob_num = n_blob
-            else:
-                blob_num = len(blobs)
-                blobs.append(blob(blob_num))
+            if cluster_num <= 0:
+                cluster_num = len(clusters)
+                clusters.append(Cluster(cluster_num))
 
-            # add x,y to the same blob
-            blobs[int(blob_num)].add_point(x, y, 1)
-            # set the reference
-            ref[y,x] = blob_num
+            # add Point to cluster
+            clusters[int(cluster_num)].add_point(x, y, 1)
+            # set reference
+            ref[y,x] = cluster_num
 
-b_size_cutoff = 3
+# minimum cluster size
+c_min_size = 3
 
 points = []
 
-for b in blobs:
+for cluster in clusters:
     # paint into control image and collect as img points
-    if b.size >= b_size_cutoff:
-        x = int(b.get_center()[0])
-        y = int(b.get_center()[1])
-        cv_image[y,x] = [0, 0, 255]
+    if cluster.size >= c_min_size:
+        x, y = cluster.get_center()
+        cv_image[int(y),int(x)] = [0, 0, 255]
+        points.append([int(x),int(y)])
 
-        points.append([x,y])
-    print(b.points)
-    print
+cv2.imshow('image',cv_image)
+cv2.waitKey(0)
 
 fx = 614.1699
 fy = 614.9002
@@ -95,11 +90,11 @@ print('object points:')
 print(object_points)
 
 if len(points) == 6:
-  img_points = np.zeros((6,2,1))
-  img_points[:,:,0] = np.array(points)
+  image_points = np.zeros((6,2,1))
+  image_points[:,:,0] = np.array(points)
   
   print('\nimage points:')
-  print(img_points[:,:,0])
+  print(image_points[:,:,0])
 
 k1 = 0.1115
 k2 = -0.1089
@@ -113,11 +108,14 @@ camera_matrix = np.array([[fx,  0, cx],
                          [ 0, fy, cy],
                          [ 0,  0,  1]])
 #print(cameraMatrix)
-retval, rvec, tvec = cv2.solvePnP(object_points, img_points, camera_matrix, dist_coeffs)
+retval, rvec, tvec = cv2.solvePnP(object_points, 
+				  image_points, 
+                                  camera_matrix, 
+                                  dist_coeffs)
 
 print('\nrotation vector:')
 print(rvec)
-print('\ntranlation vector:')
+print('\ntranslation vector:')
 print(tvec)
 
 rmat = np.zeros((3,3))
@@ -126,6 +124,13 @@ cv2.Rodrigues(rvec, rmat, jacobian=0)
 
 print('\nrotation matrix:')
 print(rmat)
+
+# Rückrechnung (noch nicht geschafft)
+t = rmat.dot(tvec)*(-1)
+H = rmat
+H[:,2] = t[:,0]
+print(H)
+
 #cv2.imshow('image',gray)
 #cv2.destroyAllWindows()
 
