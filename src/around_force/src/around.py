3b63543217c314015ecd530ed59bdf2c387db811
schmis80 <stefan.schmid96@gmail.com>
2018-08-01 19:49:56 +0200

    add assignement 7

--- src/around_force/src/around.py
+++ src/around_force/src/around.py
@@ -13,7 +13,8 @@ map_size_x=600 #cm
 map_size_y=400 #cm
 resolution = 10 # cm
 lane=1
-speed_value= 300
+speed_value= 400
+is_shutdown = False
 
 rospack = rospkg.RosPack()
 file_path=rospack.get_path('around_force')+'/src/'
@@ -28,7 +29,7 @@ def angle_diff(angle1, angle2):
     return diff
 
 def callback(msg):
-    global speed_value
+    global speed_value, matrix, is_shutdown
 
     x = msg.pose.pose.position.x
     y = msg.pose.pose.position.y
@@ -56,39 +57,59 @@ def callback(msg):
 
     steering = kp*np.arctan(y_car / (2.5*x_car))
 
-    if (x_car>0):
+    if (x_car<0):
         speed = -speed_value
+#        if (y_car>0):
+#        	steering = -np.pi/2
+#        if (y_car<0):
+#        	steering = np.pi/2
     else:
-        speed = speed_value
-        if (y_car>0):
-        	steering = -np.pi/2
-        if (y_car<0):
-        	steering = np.pi/2
+         speed = speed_value
+#        if (y_car<0):
+#        	steering = -np.pi/2
+#        if (y_car>0):
+#        	steering = np.pi/2
 
     if (steering>(np.pi)/2):
         steering = (np.pi)/2
 
     if (steering<-(np.pi)/2):
         steering = -(np.pi)/2
-    if (x_car > 0):
-        speed = max(speed_value, speed * ((np.pi/3)/(abs(steering)+1)))
 
+    if x_car > 0:
+        speed = max(speed_value, speed * ((np.pi/3)/(abs(steering)+1)))
     steering = 90 + (180/np.pi)*steering 
   #  if steering > 90:
   #      steering = 90
   #  elif steering < -90:
   #      steering = -90
+    if not is_shutdown:
+        pub_steering.publish(steering)
+        pub_speed.publish(speed)
 
-    pub_steering.publish(steering)
-    pub_speed.publish(speed)
+def laneCallback(msg):
+    global matrix
+    print msg.data
+    if msg.data == 1:
+        matrix = np.load(file_path+'matrix100cm_lane1.npy')
+    elif msg.data == 2:
+        matrix = np.load(file_path+'matrix100cm_lane2.npy')
+
+def shutdown():
+    global is_shutdown
+    pub_speed.publish(0)
+    is_shutdown = True
 
 rospy.init_node('around_force')
 localization = rospy.Subscriber('/localization/odom/1', Odometry, callback)
+rospy.Subscriber('/Schmami/lane', UInt8, laneCallback)
 
 pub_speed = rospy.Publisher('/Schmami/speed',Int16,queue_size=100)
 pub_steering = rospy.Publisher('/Schmami/steering',UInt8, queue_size = 100)
 #pub_yaw = rospy.Publisher("/desired_yaw", Float32, queue_size=100, latch=True)
 #sub_points = rospy.Subscriber("/clicked_point", PointStamped, self.lane_callback, queue_size=1)
+
+rospy.on_shutdown(shutdown)
 rospy.spin()
 
 
